69fd3027ace850008c4c078375f0e3f2d6a02325
Alex Madrazo <alex.madrazo@bgamestudios.com>
2024-05-13 10:13:01 +0200

    adding tables migration

--- productManager/migrations/1.php
+++ productManager/migrations/1.php
@@ -12,6 +12,23 @@
      echo "Connected successfully";
  }
 
-// Cerrar la conexión
+
+//  creating tables
+
+$createCategories = "
+CREATE TABLE IF NOT EXISTS categories ( 
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, category_name VARCHAR(128),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);";
+
+$createProducts = "
+CREATE TABLE IF NOT EXISTS products ( 
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, product_name VARCHAR(128),
+    product_desc VARCHAR(256), product_price DECIMAL(10,2), category_id INT(6) UNSIGNED,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);";
+
+// Close connection
 $conn->close();
 ?>
