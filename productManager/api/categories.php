bafe46b4f7d714e5350e2ad51260d9119c5527d3
Alex Madrazo <alex.madrazo@bgamestudios.com>
2024-05-13 12:14:56 +0200

    adding csv

--- productManager/api/categories.php
+++ productManager/api/categories.php
@@ -6,8 +6,22 @@ $conn = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
  if($conn->connect_error){
      die("Connection failed: " . $conn->connect_error);
  } else {
-     echo "Connected successfully";
+    //  var_dump("Connected successfully");
  }
+
+//  IDENTIFY METHOD
+$method = $_SERVER['REQUEST_METHOD'];
+
+var_dump($method);
+
+switch($method){
+    case 'GET':
+        break;
+    default: 
+        break;
+}
+
+//  CRUD METHODS
 
 // Cerrar la conexión
 $conn->close();
