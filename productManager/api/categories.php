f1be511bd56b7297d2c7a400c57418041ebec693
Alex Madrazo <alex.madrazo@bgamestudios.com>
2024-05-13 12:35:38 +0200

    adding csv

--- productManager/api/categories.php
+++ productManager/api/categories.php
@@ -5,19 +5,48 @@ $conn = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
 
  if($conn->connect_error){
      die("Connection failed: " . $conn->connect_error);
- } else {
-    //  var_dump("Connected successfully");
  }
 
 //  IDENTIFY METHOD
 $method = $_SERVER['REQUEST_METHOD'];
-
-var_dump($method);
+$response = [];
 
 switch($method){
     case 'GET':
+        $id = $_GET['id'];
+        if(isset($id)){
+            // GET SINGLE CATEGORY
+            $sql = "SELECT * FROM categories WHERE id = $id";
+            $result = $conn->query($sql);
+            $row = $result->fetch_assoc();
+            $response = ["DATA" => $row, "METHOD" => "GET", "SUCCESS" => true];
+        } else {
+            // GET ALL CATEGORIES
+            $sql = "SELECT * FROM categories";
+            $result = $conn->query($sql);
+            $rows = [];
+            while($row = $result->fetch_assoc()){
+                $rows[] = $row;
+            }
+            $response = ["DATA" => $rows, "METHOD" => "GET", "SUCCESS" => true];
+        }
+        echo(json_encode($response));
+        break;
+    case 'PUT':
+        $result = ["METHOD" => "PUT", "SUCCESS" => true];
+        echo(json_encode($result));
+        break;
+    case 'POST':
+        $result = ["METHOD" => "POST", "SUCCESS" => true];
+        echo(json_encode($result));
+        break;
+    case 'DELETE':
+        $result = ["METHOD" => "DELETE", "SUCCESS" => true];
+        echo(json_encode($result));
         break;
     default: 
+        $result = ["METHOD" => $method, "SUCCESS" => false, "ERROR" => "Method not supported"];
+        echo(json_encode($result));
         break;
 }
 
